218e1c22a73dd7ed14a50399967c5f94a5cb4088
Matheus Soares <matheusoaress2020@gmail.com>
2026-05-31 04:50:13 -0300

    Ocultando navbar ao abrir modal

--- iespflix-front/src/App.jsx
+++ iespflix-front/src/App.jsx
@@ -335,7 +335,8 @@ export default function App() {
 
     return (
         <div className="app">
-            <header className="navbar">
+            <header className={`navbar ${selecionado || trailer ? "navbar-hidden" : ""}`}>
+
                 <h1 className="logo">IespFlix</h1>
 
                 <input
